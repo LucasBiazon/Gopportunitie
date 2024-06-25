b703290eefd02dc8c7939a2bd945324287958765
Lucas <lucasbiazonpalma@gmail.com>
2024-06-25 09:27:27 -0300

    Feat: add routes logic

--- router/routes.go
+++ router/routes.go
@@ -14,5 +14,29 @@ func initializeRoutes(router *gin.Engine) {
 				"message": "Get Opening",
 			})
 		})
+
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Post Opening",
+			})
+		})
+
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Delete Opening",
+			})
+		})
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Put Opening",
+			})
+		})
+
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Get Openings",
+			})
+		})
 	}
 }
