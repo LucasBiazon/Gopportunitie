3daeca7e08ddce2cc55dd08a1fe08f32a852eb2f
Lucas <lucasbiazonpalma@gmail.com>
2024-06-26 10:13:43 -0300

    Feat: add custom Logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/LucasBiazon/Gopportunitie.git/router"
+import (
+	"github.com/LucasBiazon/Gopportunitie.git/config"
+	"github.com/LucasBiazon/Gopportunitie.git/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger := config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
